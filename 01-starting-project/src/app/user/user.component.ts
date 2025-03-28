5bfb02fcc7f7505db9117be56911e895ad4cab13
JatinbVyas <vyassjatin@gmail.com>
2025-03-29 02:29:34 +0530

    define the public variable in usercompoent class and assign random user data object to it. And the used the variable in user compoent html file using string interpolation.

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+/**
+ * This @Coomponent decorator is used to define the metadata for the component.
+ */
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,5 +15,10 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+  /**
+   * Below selectedUser is a property of the class UserComponent.
+   * And we assign reandom user from the DUMMY_USERS array to it.
+   * This selectedUser variable is used/accesible in the template/ user.component.html file to display the user details.
+   */
+  selectedUser = DUMMY_USERS[randomIndex];
 }
