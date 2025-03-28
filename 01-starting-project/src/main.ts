b416f7c05b17d41315d713db9103003e723f208a
JatinbVyas <vyassjatin@gmail.com>
2025-03-29 01:02:32 +0530

    register or we can see define new custome header component in main.ts file and due to this now it rendring in dom

--- 01-starting-project/src/main.ts
+++ 01-starting-project/src/main.ts
@@ -1,5 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { HeaderComponent } from './app/header.component';
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
+//Registering the HeaderComponent to render in dom.
+bootstrapApplication(HeaderComponent).catch((err) => console.error(err));
+
