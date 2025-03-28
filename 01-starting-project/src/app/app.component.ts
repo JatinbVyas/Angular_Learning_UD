9ff1b2fe0d56693a3e9310b08dd2c14dd05ba55f
JatinbVyas <vyassjatin@gmail.com>
2025-03-29 01:11:06 +0530

    header component registration removed from main.ts file and moved registration to app component that is root component.

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header.component';
 
 @Component({
   selector: 'jack-root',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
