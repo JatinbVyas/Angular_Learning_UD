bc57806e6c7382ed3e217d819f9b14e65fed73ad
JatinbVyas <vyassjatin@gmail.com>
2025-03-29 21:09:04 +0530

    seen the use of required configuration settings in input decorator

--- 03-ST-Input_Decorative/src/app/user/user.component.ts
+++ 03-ST-Input_Decorative/src/app/user/user.component.ts
@@ -12,9 +12,13 @@ export class UserComponent {
    * Here we seen the use of @Input decorator.
    * It is used to pass data from parent component to child component.
    * In this case avtar and name properties are accessible to app component also
+   * 
+   * Here we seen the use of required property in @Input decorator.
+   * It is used to make the input property required. If the property is not passed from parent component, it will gives error.
+   * In this case avtar and name properties are required to be passed from app component.
    */
-  @Input() avtar!: string;
-  @Input() name!: string;
+  @Input({required: true}) avtar!: string;
+  @Input({required: true}) name!: string;
 
   get imagePath() {
     return 'assets/users/' + this.avtar;
