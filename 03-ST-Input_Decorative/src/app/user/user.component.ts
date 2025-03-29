481a0314152d309f1fa074a3c4c5e35e7f4b4866
JatinbVyas <vyassjatin@gmail.com>
2025-03-29 20:55:15 +0530

    we seen the use of input decorator to pass value to html file

--- 03-ST-Input_Decorative/src/app/user/user.component.ts
+++ 03-ST-Input_Decorative/src/app/user/user.component.ts
@@ -1,8 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -13,25 +9,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
   /**
-   *Here we learn about the signal API in Angular.
-   * The signal API is a new way to manage state in Angular applications.
-   * It allows you to create reactive properties that automatically update the template when their values change.
-   * The signal API is similar to the useState and useEffect hooks in React.
-   * It is a new way to manage state in Angular applications.
-   * We can give initial value to the signal API by providing a value to the signal function in brackets.
+   * Here we seen the use of @Input decorator.
+   * It is used to pass data from parent component to child component.
+   * In this case avtar and name properties are accessible to app component also
    */
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  @Input() avtar!: string;
+  @Input() name!: string;
 
-  // We can use the computed method to create a derived signal.
-  // The computed method is similar to the useMemo hook in React.
-  // It allows you to create a signal that depends on other signals.
-  // The computed method takes a function as an argument and returns a signal that updates automatically when the values of the signals it depends on change.
-   imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
-   
+  get imagePath() {
+    return 'assets/users/' + this.avtar;
+  }
   onUserClick() {    
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    // We can use the set method to update the value of the signal.
-    // The set method is similar to the setState method in React.
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  
   }
 }
