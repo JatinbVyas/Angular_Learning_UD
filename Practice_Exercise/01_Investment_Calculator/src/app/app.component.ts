38f21d11f97e2f3dc5d4d30725d92ff700e27821
JatinbVyas <vyassjatin@gmail.com>
2025-04-01 01:33:53 +0530

    added investment results css from Maxi, added html markup and define logic to receive result data from app component.

--- Practice_Exercise/01_Investment_Calculator/src/app/app.component.ts
+++ Practice_Exercise/01_Investment_Calculator/src/app/app.component.ts
@@ -2,15 +2,25 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import { InvestmentModel } from './investmentModel.model';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  resultData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
+
   onCalculateInvestmentResults(investmentData: InvestmentModel) {
     const { initialInvestment, annualInvestment, expectedReturn, duration } =
       investmentData;
@@ -33,7 +43,6 @@ export class AppComponent {
       });
     }
 
-    console.log(annualData);
-    //return annualData;
+    this.resultData = annualData;
   }
 }
