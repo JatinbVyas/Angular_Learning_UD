b2fe8d509e28d80406e2c6b9be77a9335ed56b7e
JatinbVyas <vyassjatin@gmail.com>
2025-04-01 01:59:06 +0530

    converted normal peroperty to signal in app and user input component

--- Practice_Exercise/01_Investment_Calculator/src/app/app.component.ts
+++ Practice_Exercise/01_Investment_Calculator/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import { InvestmentModel } from './investmentModel.model';
@@ -12,14 +12,17 @@ import { InvestmentResultsComponent } from './investment-results/investment-resu
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  resultData?: {
-    year: number;
-    interest: number;
-    valueEndOfYear: number;
-    annualInvestment: number;
-    totalInterest: number;
-    totalAmountInvested: number;
-  }[];
+  resultData = signal<
+    | {
+        year: number;
+        interest: number;
+        valueEndOfYear: number;
+        annualInvestment: number;
+        totalInterest: number;
+        totalAmountInvested: number;
+      }[]
+    | undefined
+  >(undefined);
 
   onCalculateInvestmentResults(investmentData: InvestmentModel) {
     const { initialInvestment, annualInvestment, expectedReturn, duration } =
@@ -43,6 +46,6 @@ export class AppComponent {
       });
     }
 
-    this.resultData = annualData;
+    this.resultData.set(annualData);
   }
 }
--- Practice_Exercise/01_Investment_Calculator/src/app/user-input/user-input.component.ts
+++ Practice_Exercise/01_Investment_Calculator/src/app/user-input/user-input.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InvestmentModel } from '../investmentModel.model';
+import { single } from 'rxjs';
 
 @Component({
   selector: 'app-user-input',
@@ -11,18 +12,25 @@ import { InvestmentModel } from '../investmentModel.model';
 })
 export class UserInputComponent {
   @Output() onCalculateInvestment = new EventEmitter<InvestmentModel>();
-  initialInvestment: string = '0';
-  annualInvestment: string = '0';
-  expectedReturn: string = '5';
-  duration: string = '10';
+  // initialInvestment: string = '0';
+  // annualInvestment: string = '0';
+  // expectedReturn: string = '5';
+  // duration: string = '10';
+
+  // Using signals for state management
+  // This allows for more reactive programming and better performance in Angular applications.
+  initialInvestment = signal('0');
+  annualInvestment = signal('0');
+  expectedReturn = signal('5');
+  duration = signal('10');
 
   onInvestmentSubmit() {
     this.onCalculateInvestment.emit({
       // Convert string inputs to numbers using the unary plus operator
-      initialInvestment: +this.initialInvestment,
-      annualInvestment: +this.annualInvestment,
-      expectedReturn: +this.expectedReturn,
-      duration: +this.duration,
+      initialInvestment: +this.initialInvestment(),
+      annualInvestment: +this.annualInvestment(),
+      expectedReturn: +this.expectedReturn(),
+      duration: +this.duration(),
     });
   }
 }
