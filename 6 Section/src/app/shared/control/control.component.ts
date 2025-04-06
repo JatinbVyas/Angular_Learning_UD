c50843e2cfb7daf788c6b85d06dd8526f9ef3dcb
JatinbVyas <vyassjatin@gmail.com>
2025-04-07 02:07:15 +0530

    host setting in component decorator

--- 6 Section/src/app/shared/control/control.component.ts
+++ 6 Section/src/app/shared/control/control.component.ts
@@ -10,6 +10,12 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   //encapsulation: ViewEncapsulation.Emulated, // This will apply emulated encapsulation (default behavior)
   //encapsulation: ViewEncapsulation.None, // This will apply no encapsulation (global styles)
   encapsulation: ViewEncapsulation.None,
+  //host is a property that allows you to bind properties and events to the host element of the component.
+  // For example, you can set the class of the host element to 'control' using the following syntax:
+  // It means whenevet app-control is used, it will have a class property with control as value.
+  host: {
+    class: 'control',
+  },
 })
 export class ControlComponent {
   label = input.required<string>();
