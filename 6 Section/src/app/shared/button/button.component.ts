06d21284a959e8996a10f5431f175e66d64bd108
JatinbVyas <vyassjatin@gmail.com>
2025-04-07 00:42:37 +0530

    created custom button coponent using attribut selector

---
+++ 6 Section/src/app/shared/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  //The selector can be used as an attiribute. It can be used as a tag, but it is not recommended to use it as a tag. It is better to use it as an attribute.
+  // Once this appButton attribute applied to the button tag, it will be replaced with the button component html content.
+  selector: 'button[appButton]',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css',
+})
+export class ButtonComponent {}
