ecbcccd9aaa06d02ffc6fead2cf67f688ea01dac
JatinbVyas <vyassjatin@gmail.com>
2025-04-07 01:25:40 +0530

    ng content select attribute with comma separated html elements

--- 6 Section/src/app/dashboardItem/support-ticket/add-new-ticket/add-new-ticket.component.ts
+++ 6 Section/src/app/dashboardItem/support-ticket/add-new-ticket/add-new-ticket.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { ControlComponent } from '../../../shared/control/control.component';
 
 @Component({
   selector: 'app-add-new-ticket',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ControlComponent],
   templateUrl: './add-new-ticket.component.html',
   styleUrl: './add-new-ticket.component.css',
 })
--- 6 Section/src/app/shared/button/button.component.ts
+++ 6 Section/src/app/shared/button/button.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   //The selector can be used as an attiribute. It can be used as a tag, but it is not recommended to use it as a tag. It is better to use it as an attribute.
   // Once this appButton attribute applied to the button tag, it will be replaced with the button component html content.
-  selector: 'button[appButton]',
+  // By specifying the selector as 'button[appButton], a[appButton]', we can use this component as a button or an anchor tag.
+  selector: 'button[appButton], a[appButton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
