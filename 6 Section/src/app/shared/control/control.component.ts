240ccba8155478dd34c42d94ea855f7d555da4bf
JatinbVyas <vyassjatin@gmail.com>
2025-04-09 21:06:13 +0530

    seen the use of host more settings

--- 6 Section/src/app/shared/control/control.component.ts
+++ 6 Section/src/app/shared/control/control.component.ts
@@ -1,4 +1,12 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -15,8 +23,16 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   // It means whenevet app-control is used, it will have a class property with control as value.
   host: {
     class: 'control',
+    //'(click)': 'onClickHostControl()',
   },
 })
 export class ControlComponent {
+  //@HostBinding('class') className = 'control'; // This will apply the class 'control' to the host element of the component
+  private hostElement = inject(ElementRef); // This will inject the host element of the component
+  @HostListener('click') onClickHostControl() {
+    console.log('Host control clicked!');
+    console.log(this.hostElement);
+    console.log(this.hostElement.nativeElement); // This will log the host element of the component
+  } // This will listen to the click event on the host element of the component
   label = input.required<string>();
 }
