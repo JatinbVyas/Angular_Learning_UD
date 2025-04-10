88f33e821669ddb28d0de0c091dc61c5a4ea6cf4
JatinbVyas <vyassjatin@gmail.com>
2025-04-11 01:45:21 +0530

    seen the use of template variable

--- 6 Section/src/app/dashboardItem/support-ticket/add-new-ticket/add-new-ticket.component.ts
+++ 6 Section/src/app/dashboardItem/support-ticket/add-new-ticket/add-new-ticket.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './add-new-ticket.component.html',
   styleUrl: './add-new-ticket.component.css',
 })
-export class AddNewTicketComponent {}
+export class AddNewTicketComponent {
+  onSubmit(
+    titleElement: HTMLInputElement,
+    descriptionElement: HTMLTextAreaElement
+  ) {
+    const title = titleElement.value;
+    const description = descriptionElement.value;
+    console.log(title, description);
+  }
+}
