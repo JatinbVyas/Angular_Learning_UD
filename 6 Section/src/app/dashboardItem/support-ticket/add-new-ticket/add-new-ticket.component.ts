3fca8d44c1be33a3c8b35752aec0f34bcc8c334b
JatinbVyas <vyassjatin@gmail.com>
2025-04-11 02:20:50 +0530

    seen the use of viewChile signal function to access html template element.

--- 6 Section/src/app/dashboardItem/support-ticket/add-new-ticket/add-new-ticket.component.ts
+++ 6 Section/src/app/dashboardItem/support-ticket/add-new-ticket/add-new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -16,7 +16,14 @@ export class AddNewTicketComponent {
   // The 'ticketForm' is the template reference variable defined in the template.
   // The 'HTMLFormElement' is the type of the element you want to access.
   // The 'ElementRef' is a wrapper around the native element that allows you to access its properties and methods.
-  @ViewChild('ticketForm') formControl?: ElementRef<HTMLFormElement>;
+  //@ViewChild('ticketForm') formControl?: ElementRef<HTMLFormElement>;
+
+  //viewChild is a function that allows you to access a child component or directive in the template.
+  // It is used to get a reference to the form element in the template.
+  // The 'ticketForm' is the template reference variable defined in the template.
+  // The 'HTMLFormElement' is the type of the element you want to access.
+  // viewChild.required is used to make the formControl property required.
+  private formControl = viewChild<ElementRef<HTMLFormElement>>('ticketForm');
 
   onSubmit(
     titleElement: HTMLInputElement,
@@ -25,6 +32,6 @@ export class AddNewTicketComponent {
     const title = titleElement.value;
     const description = descriptionElement.value;
     console.log(title, description);
-    this.formControl?.nativeElement.reset(); // Reset the form after submission
+    this.formControl()?.nativeElement.reset(); // Reset the form after submission
   }
 }
