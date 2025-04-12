7e167293980c1c4964ab427b95d0d861a43c926d
JatinbVyas <vyassjatin@gmail.com>
2025-04-12 17:05:47 +0530

    seen the use of effect function to get subscription of signal for property in constructor.

--- 6 Section/src/app/dashboardItem/serverstatus/serverstatus.component.ts
+++ 6 Section/src/app/dashboardItem/serverstatus/serverstatus.component.ts
@@ -1,9 +1,11 @@
 import {
   Component,
   DestroyRef,
+  effect,
   inject,
   OnDestroy,
   OnInit,
+  signal,
 } from '@angular/core';
 
 @Component({
@@ -14,17 +16,28 @@ import {
   styleUrl: './serverstatus.component.css',
 })
 export class ServerstatusComponent implements OnInit {
-  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  currentStatus = signal<'online' | 'offline' | 'unknown'>('offline');
+
   private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    effect(() => {
+      console.log(
+        'From ServerStatusComponent constructor ::',
+        this.currentStatus()
+      );
+    });
+  }
+
   ngOnInit() {
     const intervalId = setInterval(() => {
       const randm = Math.random();
       if (randm > 0.5) {
-        this.currentStatus = 'online';
+        this.currentStatus.set('online');
       } else if (randm < 0.8) {
-        this.currentStatus = 'offline';
+        this.currentStatus.set('offline');
       } else {
-        this.currentStatus = 'unknown';
+        this.currentStatus.set('unknown');
       }
 
       this.destroyRef.onDestroy(() => {
