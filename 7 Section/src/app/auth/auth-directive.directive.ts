933734476b0d098ce0de63f3ce1d9bb3c3b8a864
JatinbVyas <vyassjatin@gmail.com>
2025-04-16 00:13:36 +0530

    seen the use of structural directive with the use of templateRef and viewcontainerref and notes updated.

--- 7 Section/src/app/auth/auth-directive.directive.ts
+++ 7 Section/src/app/auth/auth-directive.directive.ts
@@ -1,4 +1,11 @@
-import { Directive, effect, inject, Input } from '@angular/core';
+import {
+  Directive,
+  effect,
+  inject,
+  Input,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
 import { Permission } from './auth.model';
 import { AuthService } from './auth.service';
 
@@ -8,14 +15,23 @@ import { AuthService } from './auth.service';
 })
 export class AuthDirectiveDirective {
   @Input({ required: true }) userType!: Permission;
-
   private authService = inject(AuthService);
+
+  // This TemplateRef is the template that will be rendered when the condition is true
+  // This ViewContainerRef is the container where the template will be rendered
+  // The ViewContainerRef is a container that can hold one or more views
+  // The TemplateRef is a reference to the template that will be rendered
+  private templateRef = inject(TemplateRef);
+  private viewContainer = inject(ViewContainerRef);
+
   constructor() {
     effect(() => {
       if (this.authService.activePermission() === this.userType) {
-        console.log('Show element');
+        //createEmbeddedView creates a view from the template and adds it to the container
+        // The view is created with the context of the template, which is the data that will be used in the template
+        this.viewContainer.createEmbeddedView(this.templateRef);
       } else {
-        console.log('Hide element');
+        this.viewContainer.clear();
       }
     });
   }
