4a5fd5b3f8db6bdf93da1fb8d260ef8761ee4cf3
JatinbVyas <vyassjatin@gmail.com>
2025-04-16 00:46:04 +0530

    seen the uses of hostdirectives settings in component decorator

--- 7 Section/src/app/auth/auth.component.ts
+++ 7 Section/src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AuthService } from './auth.service';
+import { LogDirective } from '../log.directive';
 
 @Component({
   selector: 'app-auth',
@@ -9,6 +10,11 @@ import { AuthService } from './auth.service';
   imports: [FormsModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
+  //hostdirectives is a property that allows you to add directives to the host element of the component
+  //hostdirectives is an array of directives that will be added to the host element of the component
+  //Now this LogDirective will be added to the whole auth component
+  //This means that the LogDirective will be applied to the whole auth component, wherever clicked on auth compoent console log statment will print element object details.
+  hostDirectives: [LogDirective],
 })
 export class AuthComponent {
   email = signal('');
