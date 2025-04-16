c2f544ebc944d10a47b24f227365f3978321c9ff
JatinbVyas <vyassjatin@gmail.com>
2025-04-17 00:09:20 +0530

    made changes in pipe to return truncated values and updated notes.

--- 8 Section/src/app/temprature.pipe.ts
+++ 8 Section/src/app/temprature.pipe.ts
@@ -35,15 +35,4 @@ export class TempraturePipe {
     }
     return `${outputValue.toFixed(2)}${symbol}`;
   }
-  // transform(value: number, unit: string): string {
-  //   if (unit === 'C') {
-  //     return `${value}°C`;
-  //   } else if (unit === 'F') {
-  //     return `${(value * 9) / 5 + 32}°F`;
-  //   } else if (unit === 'K') {
-  //     return `${value + 273.15}K`;
-  //   } else {
-  //     return `${value}°C`;
-  //   }
-  // }
 }
