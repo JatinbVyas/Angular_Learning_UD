182ce6d1ca754bd3fd1e4d7e950db997aadb976f
JatinbVyas <vyassjatin@gmail.com>
2025-04-16 23:36:40 +0530

    cerated owe own pipe with more configuration options.

--- 8 Section/src/app/temprature.pipe.ts
+++ 8 Section/src/app/temprature.pipe.ts
@@ -5,8 +5,35 @@ import { Pipe } from '@angular/core';
   standalone: true,
 })
 export class TempraturePipe {
-  transform(value: any, ...args: any[]) {
-    return value + '°C';
+  transform(
+    value: string | number,
+    inputType: 'cel' | 'fah',
+    outputType: 'cel' | 'fah'
+  ) {
+    let val: number;
+    if (typeof value === 'string') {
+      val = parseFloat(value);
+    } else {
+      val = value;
+    }
+
+    let outputValue: number;
+    if (inputType === 'cel' && outputType === 'fah') {
+      outputValue = (val * 9) / 5 + 32;
+    } else if (inputType === 'fah' && outputType === 'cel') {
+      outputValue = ((val - 32) * 5) / 9;
+    } else {
+      outputValue = val;
+    }
+
+    let symbol: '°C' | '°F';
+
+    if (!outputType) {
+      symbol = inputType === 'cel' ? '°C' : '°F';
+    } else {
+      symbol = outputType === 'cel' ? '°C' : '°F';
+    }
+    return `${outputValue}${symbol}`;
   }
   // transform(value: number, unit: string): string {
   //   if (unit === 'C') {
