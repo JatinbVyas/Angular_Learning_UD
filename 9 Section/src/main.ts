a735708793ceb954aa334caffb488855ea59076d
JatinbVyas <vyassjatin@gmail.com>
2025-04-24 23:05:08 +0530

    made service injectable from main ts file using providers

--- 9 Section/src/main.ts
+++ 9 Section/src/main.ts
@@ -1,5 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { TaskService } from './app/tasks/tasks.service';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  //providers is an array of providers that are available to the entire application
+  //This way TaskService is injected into the entire applications
+  providers: [TaskService],
+}).catch((err) => console.error(err));
